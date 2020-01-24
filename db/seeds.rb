42f9f3d961c8e6f765f397ccb9206a3635a19c55
github username <github email address>
2020-01-24 10:09:52 -0500

    finished CLI project (for now)

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,10 @@ Action.destroy_all
 Dev.destroy_all
 Result.destroy_all
 
-freelance = Action.create(action_name: "Freelance")
-eat_food = Action.create(action_name: "Eat food")
-buy_a_mac = Action.create(action_name: "Buy a Macbook Pro")
+Action.create(action_name: "Freelance")
+Action.create(action_name: "Eat food")
+Action.create(action_name: "Armed Robbery")
+Action.create(action_name: "Buy a Macbook Pro")
 
 ##testing Github ##
 puts "done seeding!"
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -12,6 +12,76 @@ class CommandLineInterface
         name = prompt.ask(string, required: true)
         name.chomp
     end
+
+    def start_menu
+        choice = select_prompt("", start_menu_options)
+        case choice
+        when "New Game"
+            new_game
+        when "Load Game"
+            load_game
+        when "Exit"
+            exit_game
+        end
+    end 
+
+    # Menu options
+    def start_menu_options
+        ["New Game", "Load Game", "Exit"]
+    end
+
+    def new_game
+        greet
+        character_creation
+        story_setup
+        hi_new_person
+    end
+
+    def load_game
+        dev_menu_options = Dev.all.map(&:dev_name)
+        choice = select_prompt("Pick a dev to play as!", dev_menu_options)
+        case choice 
+        when choice 
+            self.dev = locate_dev_instance(choice)
+        end
+        puts "Welcome Back! now lets get the grind on!"
+    end
+
+    def story_setup  #(THEO)
+        name = "#{self.dev.dev_name.capitalize}"
+        puts "In the future, there is a broke software developer named #{self.dev.dev_name}..."
+        sleep 1.5
+        puts "#{self.dev.dev_name} has just graduated from a magically stressful job training program..."
+        sleep 1.5
+        puts "#{self.dev.dev_name} wants to be somebody, but has no job... no money... and absolutely no game..."
+        sleep 1.5
+        puts "In order to stop being such a loser and have a real life, #{self.dev.dev_name} will have to..."
+        sleep 1.5
+        puts "You need to Hustle hard..."
+        sleep 1.5
+        puts "chuck more wood than a woodchuck.."
+        sleep 1.5
+        puts "risk everything"
+        sleep 1.5 
+        puts "create a CLI app..possibly?"
+        sleep 1.5
+        puts ""
+        sleep 1.5
+    end 
+
+
+    def locate_dev_instance(name)
+        Dev.find_by(dev_name: name)
+    end
+    
+    def exit_game
+        sleep 1.5
+        puts "I didn't want to play with you anyway..."
+        sleep 1
+        puts "Butthead"
+        sleep 1
+        exit!
+    end
     
     #---------------------------Char Creation-------------------------#
     def character_creation
@@ -20,56 +90,66 @@ class CommandLineInterface
             notice("A Developer already got that name! \nPlease choose a different name... and be original PLEASE!", :red)
             return character_creation
         else
-            @dev = Dev.create(dev_name: dev_name, cash: 50)
+            self.dev = Dev.create(dev_name: dev_name, cash: 50)
             system("clear")
         end
     end
 
     def hi_new_person
-        puts "Nice to meet you #{@dev.dev_name}! 🤝"
+        puts "Nice to meet you #{self.dev.dev_name}! 🤝"
         sleep 3
-        puts "Oh hey.. you just found $#{@dev.cash} on the floor!"
+        puts "Oh hey.. you just found $#{self.dev.cash} on the floor!"
         puts "....."
         sleep 2
         puts "....."
         sleep 2
         puts "well? nobody's looking, go pick it up!"
         sleep 2
-        puts "Congrats! now you have $#{@dev.cash}. Started from the bottom, nothing changed....except this."
+        puts "Congrats! now you have $#{self.dev.cash}. Started from the bottom, nothing changed....except this."
     end
 
     #--------------------Action Menu ---------------------------#
     def action_options
         ["Lets make some money Freelancing!",
         "I'm hungry, lets grab something to eat",
+        "I HATE THIS! IM TURNING TO A LIFE OF CRIME MUAHAHA!!!!!",
         "I want to buy that new Macbook!"]
     end
 
     def freelance
-        Result.create(action_id: Action.first.id, dev_id: @dev.id, payment: rand(50..80))
+        Result.create(action_id: Action.first.id, dev_id: self.dev.id, payment: rand(50..80))
     end
 
     def freelancing
-        @dev.cash += freelance.payment
-        @dev.save
+        self.dev.cash += freelance.payment
+        self.dev.save
     end
 
     def eat
-        Result.create(action_id: Action.second.id, dev_id: @dev.id, payment: rand(10..30))
+        Result.create(action_id: Action.second.id, dev_id: self.dev.id, payment: rand(10..30))
     end
 
     def eat_food
-        @dev.cash -= eat.payment
-        @dev.save
+        self.dev.cash -= eat.payment
+        self.dev.save
+    end
+
+    def steal
+        Result.create(action_id: Action.third.id, dev_id: self.dev.id, payment: rand(150000..350000))
+    end
+
+    def armed_robbery
+        self.dev.cash += steal.payment
+        self.dev.save
     end
 
     def mac
-        Result.create(action_id: Action.last.id, dev_id: @dev.id, payment: 1000)
+        Result.create(action_id: Action.last.id, dev_id: self.dev.id, payment: 1000)
     end
 
     def buy_mac
-        @dev.cash -= mac.payment
-        @dev.save
+        self.dev.cash -= mac.payment
+        self.dev.save
     end
 
     def action_menu
@@ -77,21 +157,62 @@ class CommandLineInterface
         case choice
         when "Lets make some money Freelancing!"
             freelancing
-            puts "You made money!!!! good job!!! it wasn't software engineering, but hey, beggars can't be choosers."
+            puts "You made money!!!! good job!!! it wasn't the dream job...? but hey, beggars can't be choosers."
+            sleep 3
+            system("clear")
             main_story
         when "I'm hungry, lets grab something to eat"
-            if (@dev.cash - eat.payment) < 0
+            eat_num = rand(1..5)
+            if (self.dev.cash - eat.payment) < 0
                 puts "Are you crazy?! you want to stay broke dont you?"
+                sleep 1
                 puts "You probably like to work for free, dont you?"
+                sleep 3
+                system("clear")
                 main_story
             else
-                eat_food
-                main_story
+                if eat_num == 1
+                    puts "You had some delicious food! it soothed your soul and gave you hope for pushing forward!"
+                    eat_food
+                    main_story
+                elsif eat_num == 2
+                    puts "You had some ramen... because.. ramen is nice."
+                    eat_food
+                    main_story
+                elsif eat_num == 3
+                    puts "you had a salad today! because health is good!!!"
+                    eat_food
+                    main_story
+                elsif eat_num == 4
+                    puts "Yogurt, gogurt, all the gurts. great food!!! Delicious probiotics!"
+                    eat_food
+                    main_story
+                elsif eat_num == 5
+                    puts "Eat some chinese food, because... ur craving MSG.. enough to kill an elephant"
+                    eat_food
+                    main_story
+                end
             end
+
+        when "I HATE THIS! IM TURNING TO A LIFE OF CRIME MUAHAHA!!!!!"
+            puts "You turn to a life of crime!"
+            crime_num = rand(1..10)
+            sleep 1
+                if crime_num <= 3
+                    armed_robbery
+                    crime_ending_good
+                else
+                    self.dev.destroy
+                    crime_ending_bad
+                end
+
         when "I want to buy that new Macbook!"
-            if (@dev.cash - mac.payment) < 0
+            if (self.dev.cash - mac.payment) < 0
                 puts "I can't afford that yet!"
+                sleep 1
                 puts "time to get back on the grind!"
+                sleep 3
+                system("clear")
                 main_story
             else
                 buy_mac
@@ -109,14 +230,45 @@ class CommandLineInterface
 
 
     def status
-        puts "#{@dev.dev_name.capitalize}, this poor dev.."
-        puts "$#{@dev.cash}"
-        puts ""
+        if self.dev.cash > 20000
+            puts "#{@dev.dev_name.capitalize}, this...RICH dev!!!"
+            puts "$#{@dev.cash}"
+            puts ""
+        else
+            puts "#{@dev.dev_name.capitalize}, this poor dev.."
+            puts "$#{@dev.cash}"
+            puts ""
+        end
+    end
+
+
+    def crime_ending_good
+        puts "..."
+        puts "..."
+        sleep 2
+        puts ".....!!!!!"
+        sleep 1
+        puts "Holy crap, you pulled off the bank heist successfully!!"
+        sleep 1
+        puts "Well now that you're a multi-thousandaire... you got no money woes! congrats!"
+        puts "This wasn't what I expected to happen to you... well Good job!"
+        sleep 1
+        puts "Anything with a big money win is a Win in my book. CONGRATS!"
+        puts "Now maybe you can help some other poor devs!"
+        exit!
+    end
+
+    def crime_ending_bad
+        puts "sadly... You're BAD at this too! and.. one day, one of your heists went wrong!"
+        sleep 1
+        puts "You're stuck in a gunfight with the authorities! and their guns are better"
+        puts "You're dead. The end."
+        exit!
     end
 
     def game_ending
         puts "Congratulations! you bought a mac! now onto a brighter future!"
-        puts "DUN DUN DUN! YOU WIN!!!!!!!"
+        puts "DUN DUN DUN! YOU WIN!!!!!!! .... or did you? wait this mac is another 2011?! DARNIT!"
         exit!
     end
 
@@ -125,31 +277,35 @@ class CommandLineInterface
         title = TTY::Font.new(:doom)
         puts title.write("Broke Dev RPG")
         puts "
-        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-        X///::::::::::::::................................................................::::::://////////X
-        X///:::::::::::::..................................................................:::::://////////X
-        X////::::::::::....................................................................:::::://////////X
-        X////::::::::::..................................................................::::::::://///////X
-        X////::::::::...................................................................:::::::::://///////X
-        X////::::::::....................................................................::::::::::////////X
-        X////:::::::::......................................................................::::::::///////X
-        X///:::::::::.....................................................................::::::::::://////X
-        X///::::::::::............................o++/-..--..--..```` ....................::::::::::://////X
-        X//:::::/:::::......................`````oddhhhhhhhyo///:-.`  `......................:::::::://////X
-        X///::::y/................/+++/:-......-:/hmddddhysssossssoo+/:-..--.```...............-::::://////X
-        X//:::::/y............/syyyyyo++ooo++///o+dNNNmddmdyssssyyyhhyyo++/:-```..................::://////X
-        X//::::::/y.......:++ydyyyyso/://///::::+odMMMmmmmmhddhhmyyhhdhysyhdho:.................::::///////X
-        X//......./y......dMNmhhhhyso+///:----:/oshMMMNmmmNNmhdNdhdyyyhdhdmmmmhy+.........:-::::::::///////X
-        X///.......+s..../mMNmhhdysso+++/:--/+osssyMMMMNNNNmmdNddmhoosyhmmhmmmmmdy+......::::::::::////////X
-        X///........+s....dMMNNmdysoo+ys:+so+ossyhmMMMMMMNNNmNmNmmhssssdmmhddmmmmmdy:..::::::::::://///////X
-        X////........oo...yNMNNNdhyso++++/++oyyyydNMMMMMMMNNNNNNNmdhsoydmdhhhhdmmmmdh/-:::::://////////////X
-        X////.........o+..sMMMNNmdhso++++++ossyhhdmMMMMMMMNNNNNNNmmdhhhyhNdhhhhddmmmdds:::::/yhhyyssoo+++++X
-        X///////.......s+..hMMMNdyhdhso+++sossyhdmNNMMMMMMNNNNNNNNNmmdmhhmdhhdhhhhhmhmmh+:::ohddddhhyysooooX
-        X/////////......yos/mMMMmmmhsssyshNNNNNNds/+NMMMMMMNNNNNNNNNmNddNmhhsddhhyhdddmmms:+hddddhhhyyssssdX
-        X:::::::::::----:yhsoNMMMNmmddNNNhdNMNo/::-.-sNMMMMMNNNNNNNNNNdNmdmhoymmddddmdmmmmhyhddhhhhyyyyysdMX
-        X:::::::::::::::/ohhyydddhhhhhhddddmmmso////os+yMMMMMMMMMNMMNNMmdmmdysdmmmddddddmmmmhhhhhyyyyyyydMNX
-        X.....------:::/+oyhhhddddddhdddhddmdddhddddddddmmNMMMMMMMMNMMNmNmddhyydmdddhhddddmmmhhhyyyyyyyhMNNX
-        XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n\n\n\n\n"
+        
+        `-:::-.`                                         
+        -ohmmmmmmmdy+-                                      
+     :ydmmmmmmmmmmmmmmh+`                                   
+    :mmmmmmmmmmmmmmmmmmmh.                                  
+    +mmmmmmmmmmmmmmmmmmmm-                                  
+    .mmmmmmmmmmmmmdmmmds-                                   
+    `:+syyhyyso+/:/:::`                                     
+    `...-----..../h+..                                      
+     ............---.`                                      
+      ..............`                                       
+        `........``                                         
+         ...--.`                                            
+        `//////:                    /.                      
+        -///////`                   o/                      
+        ////////:`                  +/                      
+       ./////////-.```````          +/                      
+       ://///////-....`  ...........s/                      
+      `//////////:``  ```ooooooooooos-```                   
+      .///////////    -/////////////////-                   
+      :///////////.   .---//:-----//:---.                   
+      -/ssssssssss+/::`   //-     :/:                       
+       `hmmmmmmmmmmmmm`   //-     :/:                       
+      `-:oooooo+::/mmh    //-     :/:                       
+       .//////-`  .mmo    //-     :/:                       
+        //////.   .mm-    //-     :/:                       
+        //////.   .mm     //-     :/:                       
+        //////.   .md/.`  //-     :/:                       
+        ```````   `::::.  ```     ```                       "
     end
 
 
@@ -172,13 +328,11 @@ class CommandLineInterface
     end
 
      #-------------------Story Board--------------------------------#
-     def run 
+     def run
         opening_title
-        sleep 3.0
+        sleep 3
         system("clear")
-        greet
-        character_creation
-        hi_new_person
+        start_menu
         main_story
     end
 end
